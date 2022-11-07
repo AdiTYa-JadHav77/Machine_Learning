43ece0848a3dcacba3497660b2a3928bbb9b0d09
unknown <adityajadhav0502@gmail.com>
2022-11-07 23:31:31 +0530

    Completed exception init

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from housing.logger import logging
+import sys
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("We are testing custom messages")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging info")
     return "Starting Machine Learning Project"
 
 if __name__ == "__main__":
